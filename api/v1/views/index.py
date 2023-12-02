ff1f7bbd9bf840eb6092a031a09908b209d29162
Niamask <niamaskay@gmail.com>
2023-12-02 08:04:58 -0800

    3. Status of your API

---
+++ api/v1/views/index.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module contains endpoint(route) status
+"""
+
+from models import storage
+from flask import Flask
+from flask import jsonify
+from api.v1.views import app_views
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """
+    Returns a JSON status
+    """
+    return jsonify({"status": "OK"})
