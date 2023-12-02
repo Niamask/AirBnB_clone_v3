aa1eb1f155b464f42182207915537b7a95bc6a00
Niamask <niamaskay@gmail.com>
2023-12-02 15:40:17 -0800

    4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,7 +7,7 @@ from models import storage
 from flask import Flask
 from flask import jsonify
 from api.v1.views import app_views
-
+from sqlalchemy.sql import text
 
 @app_views.route('/status', strict_slashes=False)
 def status():
@@ -15,3 +15,15 @@ def status():
     Returns a JSON status
     """
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def count():
+    """
+    Retrieves the number of each objects by type
+    """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
