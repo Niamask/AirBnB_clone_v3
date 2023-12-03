32d87ea30a7fdd18ddd3493b4768bda7e50736b6
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-12-02 22:38:01 -0800

    fixed error

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,10 +4,17 @@ This module contains endpoint(route) status
 """
 
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from flask import Flask
 from flask import jsonify
 from api.v1.views import app_views
 from sqlalchemy.sql import text
+
 
 @app_views.route('/status', strict_slashes=False)
 def status():
@@ -16,14 +23,15 @@ def status():
     """
     return jsonify({"status": "OK"})
 
+
 @app_views.route('/stats', strict_slashes=False)
 def count():
     """
     Retrieves the number of each objects by type
     """
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
+    return jsonify({"amenities": storage.count(Amenity),
+                    "cities": storage.count(City),
+                    "places": storage.count(Place),
+                    "reviews": storage.count(Review),
+                    "states": storage.count(State),
+                    "users": storage.count(User)})
