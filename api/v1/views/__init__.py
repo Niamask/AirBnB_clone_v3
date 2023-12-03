9acafd8dd19d7dc5be626c741a7c098398bb1bab
Niamask <niamaskay@gmail.com>
2023-12-03 11:50:58 -0800

    Revert "8. Amenity"
    This reverts commit d7bc3084b7e070c6da45f646ad693dd963523b73.

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,5 +7,8 @@ from api.v1.views import *
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
+<<<<<<< HEAD
 from api.v1.views.amenities import *
 from api.v1.views.users import *
+=======
+>>>>>>> parent of d7bc308... 8. Amenity
