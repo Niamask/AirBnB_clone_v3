58d352fd6b64b2d43d57fb7565a213703a7cc07c
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-12-04 08:41:33 -0800

    trying with reviews3

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -31,7 +31,7 @@ def get_review(review_id):
     return jsonify(review.to_dict())
 
 
-@app_views.route('/reviews/<string:review_id>', methods=['DELETE'],
+@app_views.route('/reviews/<review_id>', methods=['DELETE'],
                  strict_slashes=False)
 def del_review(review_id):
     """ delete review by id"""
@@ -43,7 +43,7 @@ def del_review(review_id):
     return jsonify({}), 200
 
 
-@app_views.route('/places/<string:place_id>/reviews', methods=['POST'],
+@app_views.route('/places/<place_id>/reviews', methods=['POST'],
                  strict_slashes=False)
 def create_obj_review(place_id):
     """ create new instance """
@@ -66,13 +66,13 @@ def create_obj_review(place_id):
     return (jsonify(obj.to_dict()), 201)
 
 
-@app_views.route('/review/<string:review_id>', methods=['PUT'],
+@app_views.route('/review/<review_id>', methods=['PUT'],
                  strict_slashes=False)
 def post_review(review_id):
     """ update review  """
     if not request.get_json():
         return abort(400, "Not a JSON")
-    obj = storage.get(Review, user_id)
+    obj = storage.get(Review, review_id)
     if obj is None:
         abort(404)
     for ke, value in request.get_json().items():
